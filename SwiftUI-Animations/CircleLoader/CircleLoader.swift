5b028fc626eae00fba38fa13d4072ec0ee01e668
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-05 21:22:21 +0530

    Circle Loaded added

--- SwiftUI-Animations/CircleLoader/CircleLoader.swift
+++ SwiftUI-Animations/CircleLoader/CircleLoader.swift
@@ -9,8 +9,72 @@
 import SwiftUI
 
 struct CircleLoader: View {
+    
+    // MARK:- variables
+    let circleTrackGradient = LinearGradient(gradient: .init(colors: [Color.circleTrackStart, Color.circleTrackEnd]), startPoint: .leading, endPoint: .bottomLeading)
+    let circleRoundGradient = LinearGradient(gradient: .init(colors: [Color.circleRoundStart, Color.circleRoundEnd]), startPoint: .topLeading, endPoint: .trailing)
+    
+    let trackerRotation: Double = 2
+    let animationDuration: Double = 0.75
+    
+    @State var isAnimating: Bool = false
+    @State var circleStart: CGFloat = 0.17
+    @State var circleEnd: CGFloat = 0.325
+    
+    @State var rotationDegree: Angle = Angle.degrees(0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.white
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack {
+                Circle()
+                    .stroke(style: StrokeStyle(lineWidth: 20))
+                    .fill(circleTrackGradient)
+                Circle()
+                    .trim(from: circleStart, to: circleEnd)
+                    .stroke(style: StrokeStyle(lineWidth: 15, lineCap: .round))
+                    .fill(circleRoundGradient)
+                    .rotationEffect(self.rotationDegree)
+            }.frame(width: 200, height: 200)
+            .onAppear() {
+                animateLoader()
+                Timer.scheduledTimer(withTimeInterval: trackerRotation * animationDuration + (animationDuration), repeats: true) { (mainTimer) in
+                    animateLoader()
+                }
+            }
+        }
+    }
+    
+    // MARK:- functions
+    func getRotationAngle() -> Angle {
+        return .degrees(360 * self.trackerRotation) + .degrees(120)
+    }
+    
+    func animateLoader() {
+        withAnimation(Animation.spring(response: animationDuration * 2 )) {
+            self.rotationDegree = .degrees(-57.5)
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: false) { _ in
+            withAnimation(Animation.easeInOut(duration: trackerRotation * animationDuration)) {
+                self.rotationDegree += getRotationAngle()
+            }
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: animationDuration * 1.25, repeats: false) { _ in
+            withAnimation(Animation.easeOut(duration: (trackerRotation * animationDuration) / 2.25 )) {
+                self.circleEnd = 0.925
+            }
+        }
+        
+        Timer.scheduledTimer(withTimeInterval: trackerRotation * animationDuration, repeats: false) { _ in
+            self.rotationDegree = .degrees(47.5)
+            withAnimation(Animation.easeOut(duration: animationDuration)) {
+                self.circleEnd = 0.325
+            }
+        }
     }
 }
 
