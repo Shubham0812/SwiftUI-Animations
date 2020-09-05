e564ab610cd2dd67d08eb5c975229dac0b7c59c9
Shubham0812 <singh.shubham0812@gmail.com>
2020-09-05 21:22:06 +0530

    Colors added

---
+++ SwiftUI-Animations/CircleLoader/CircleLoader.swift
@@ -0,0 +1,21 @@
+//
+//  CircleLoader.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 05/09/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleLoader_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleLoader()
+    }
+}
--- SwiftUI-Animations/Utils/Colors.swift
+++ SwiftUI-Animations/Utils/Colors.swift
@@ -21,6 +21,12 @@ extension Color {
     static let expandingBackground: Color = Color(r: 3, g: 247, b: 235)
     static let expandingAccent: Color = Color(r: 186, g: 38, b: 75)
     
+    static let circleTrackStart: Color = Color(r: 237, g: 242, b: 255)
+    static let circleTrackEnd: Color = Color(r: 235, g: 248, b: 255)
+    
+    static let circleRoundStart: Color = Color(r: 71, g: 198, b: 255)
+    static let circleRoundEnd: Color = Color(r: 90, g: 131, b: 255)
+    
     init(r: Double, g: Double, b: Double) {
         self.init(red: r / 255.0, green: g / 255.0, blue: b / 255.0)
     }
