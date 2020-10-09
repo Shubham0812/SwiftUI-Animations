616eb9f967fcaeccc6202c7b182f324aed9e3529
Shubham0812 <singh.shubham0812@gmail.com>
2020-10-09 21:11:44 +0530

    Submit view added

--- SwiftUI-Animations/SubmitView/SubmitView.swift
+++ SwiftUI-Animations/SubmitView/SubmitView.swift
@@ -9,8 +9,83 @@
 import SwiftUI
 
 struct SubmitView: View {
+    
+    // MARK:- variables
+    let animationDuration: TimeInterval = 0.45
+    let trackerRotation: Double = 2.585
+    
+    @State var isAnimating: Bool = false
+    @State var taskDone: Bool = false
+    
+    @State var submitScale: CGFloat = 1
+    
+    // MARK:- views
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            ZStack {
+                RoundedRectangle(cornerRadius: self.isAnimating ? 46 : 20, style: .circular)
+                    .fill(Color.submitColor)
+                    .frame(width: self.isAnimating ? 92 : 300, height: 92)
+                    .scaleEffect(submitScale, anchor: .center)
+                    .onTapGesture {
+                        toggleIsAnimating()
+                        animateButton()
+                        resetSubmit()
+                        Timer.scheduledTimer(withTimeInterval:  trackerRotation * 0.95, repeats: false) { _ in
+                            self.taskDone.toggle()
+                        }
+                    }
+                if (self.isAnimating) {
+                    RotatingCircle(trackerRotation: trackerRotation, timerInterval:  trackerRotation * 0.91)
+                }
+                Tick(scaleFactor: 0.4)
+                    .trim(from: 0, to: self.taskDone ? 1 : 0)
+                    .stroke(style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                    .foregroundColor(Color.white)
+                    .frame(width: 16)
+                    .offset(x: -4, y: 4)
+                    .animation(.easeOut(duration: 0.35))
+                Text("Submit")
+                    .font(.system(size: 32, weight: .bold, design: .monospaced))
+                    .foregroundColor(Color.white)
+                    .opacity(self.isAnimating ? 0 : 1)
+                    .animation(Animation.easeOut(duration: animationDuration))
+                    .scaleEffect(self.isAnimating ? 0.7 : 1)
+                    .animation(Animation.easeOut(duration: animationDuration))
+            }
+        }
+    }
+    
+    // MARK:- functions
+    func animateButton() {
+        expandButton()
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { _ in
+            expandButton()
+        }
+    }
+    
+    func expandButton() {
+        withAnimation(Animation.linear(duration: 0.5)) {
+            self.submitScale = 1.35
+        }
+        withAnimation(Animation.linear(duration: 0.5).delay(0.5)) {
+            self.submitScale = 1
+        }
+    }
+    
+    func resetSubmit() {
+        Timer.scheduledTimer(withTimeInterval: trackerRotation * 0.95 + animationDuration * 3.5, repeats: false) { _ in
+            toggleIsAnimating()
+            self.taskDone.toggle()
+        }
+    }
+    
+    func toggleIsAnimating() {
+        withAnimation(Animation.spring(response: animationDuration * 1.25, dampingFraction: 0.9, blendDuration: 1)){
+            self.isAnimating.toggle()
+        }
     }
 }
 
--- SwiftUI-Animations/SubmitView/Support Shapes/RotatingCircle.swift
+++ SwiftUI-Animations/SubmitView/Support Shapes/RotatingCircle.swift
@@ -9,13 +9,64 @@
 import SwiftUI
 
 struct RotatingCircle: View {
+    
+    // MARK:- variables
+    @State var isAnimating: Bool = false
+    @State var rotationAngle: Angle = .degrees(0)
+    @State var circleScale: CGFloat = 0.5
+    @State var xOffset: CGFloat = 30
+    @State var yOffSet: CGFloat = 0
+    @State var opacity: Double = 1
+    
+    let trackerRotation: Double
+    let timerInterval: TimeInterval
+    
+    // MARK:- views
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .fill(Color.white)
+            .frame(width: 40)
+            .offset(x: xOffset, y: yOffSet)
+            .rotationEffect(rotationAngle)
+            .scaleEffect(circleScale)
+            .opacity(opacity)
+            .onAppear() {
+                //                Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { _ in
+                withAnimation(Animation.easeOut(duration: 0.2)) {
+                    self.circleScale = 1
+                    self.xOffset = 130
+                }
+                withAnimation(Animation.linear(duration: timerInterval)) {
+                    self.rotationAngle = getRotationAngle()
+                }
+                Timer.scheduledTimer(withTimeInterval: timerInterval, repeats: false) { _ in
+                    withAnimation(Animation.easeOut(duration: 0.2)) {
+                        self.circleScale = 0.25
+                        self.xOffset = 60
+                        self.yOffSet = -40
+                    }
+                }
+                Timer.scheduledTimer(withTimeInterval: timerInterval + 0.05, repeats: false) { _ in
+                    withAnimation(Animation.default) {
+                        self.opacity = 0
+                    }
+                }
+            }
+        //            }
+    }
+    
+    // MARK:- functions
+    func getRotationAngle() -> Angle {
+        return .degrees(360 * self.trackerRotation)
     }
 }
 
 struct RotatingCircle_Previews: PreviewProvider {
     static var previews: some View {
-        RotatingCircle()
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            RotatingCircle(trackerRotation: 2.4, timerInterval:  2.4 * 0.91)
+        }
     }
 }
