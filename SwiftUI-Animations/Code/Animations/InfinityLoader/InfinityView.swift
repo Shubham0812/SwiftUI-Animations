d002a20859e78419dd988db960923cf968fed7cd
Shubham0812 <singh.shubham0812@gmail.com>
2020-12-04 16:06:54 +0530

    Added Infinity Loader

---
+++ SwiftUI-Animations/Code/Animations/InfinityLoader/InfinityView.swift
@@ -0,0 +1,21 @@
+//
+//  InfinityView.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 03/12/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfinityView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfinityView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfinityView()
+    }
+}
---
+++ SwiftUI-Animations/Code/Animations/InfinityLoader/Support Shapes/InfinityShape.swift
@@ -0,0 +1,21 @@
+//
+//  InfinityShape.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 03/12/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfinityShape: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfinityShape_Previews: PreviewProvider {
+    static var previews: some View {
+        InfinityShape()
+    }
+}
