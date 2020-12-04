db6c7fd6d065d633acc65ecc5f205e71e850631b
Shubham0812 <singh.shubham0812@gmail.com>
2020-12-04 16:07:09 +0530

    Workspace Updated

--- SwiftUI-Animations/Code/Animations/InfinityLoader/InfinityView.swift
+++ SwiftUI-Animations/Code/Animations/InfinityLoader/InfinityView.swift
@@ -9,8 +9,52 @@
 import SwiftUI
 
 struct InfinityView: View {
+    
+    // MARK:- variables
+    let animationDuration: TimeInterval = 0.2
+    let strokeWidth: CGFloat = 20
+    let animationCap: CGFloat = 1.205
+    
+    
+    @State var strokeStart: CGFloat = 0
+    @State var strokeEnd: CGFloat = 0
+    @State var additionalLength: CGFloat = 0
+    
+    // MARK:- views
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.all)
+            InfinityShape()
+                .stroke(style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round, lineJoin: .round))
+                .foregroundColor(.white)
+                .shadow(color: Color.white,radius: 4)
+                .overlay(
+                    InfinityShape()
+                        .trim(from: strokeStart, to: strokeEnd)
+                        .stroke(style: StrokeStyle(lineWidth: strokeWidth - 0.5, lineCap: .round, lineJoin: .round))
+                        .foregroundColor(.materialBlack)
+                        .shadow(color: Color.white, radius: 5)
+                )
+        }.onAppear() {
+            Timer.scheduledTimer(withTimeInterval: animationDuration, repeats: true) { _ in
+                withAnimation(Animation.linear(duration: animationDuration)) {
+                    strokeEnd += 0.05
+                    strokeStart = strokeEnd - (0.05 + additionalLength)
+                }
+                
+                // reset values
+                if (strokeEnd >= animationCap) {
+                    strokeEnd = 0
+                    additionalLength = 0
+                    strokeStart = 0
+                }
+            }
+            
+            Timer.scheduledTimer(withTimeInterval: animationDuration * 3, repeats: true) { _ in
+                additionalLength += 0.015
+            }
+        }
     }
 }
 
--- SwiftUI-Animations/Code/Animations/InfinityLoader/Support Shapes/InfinityShape.swift
+++ SwiftUI-Animations/Code/Animations/InfinityLoader/Support Shapes/InfinityShape.swift
@@ -8,14 +8,25 @@
 
 import SwiftUI
 
-struct InfinityShape: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct InfinityShape: Shape {
+    
+    // MARK:- functions
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX - 100, y: rect.midY + 72))
+        
+        path.addCurve(to: CGPoint(x: rect.midX - 100, y: rect.midY - 72), control1: CGPoint(x: rect.midX - 200, y: rect.midY + 72), control2: CGPoint(x: rect.midX - 200, y: rect.midY - 72))
+        path.addCurve(to: CGPoint(x: rect.midX + 100, y: rect.midY + 72), control1: CGPoint(x: rect.midX , y: rect.midY - 72), control2: CGPoint(x: rect.midX, y: rect.midY + 72))
+        path.addCurve(to: CGPoint(x: rect.midX + 100, y: rect.midY - 72), control1: CGPoint(x: rect.midX + 200, y: rect.midY + 72), control2: CGPoint(x: rect.midX + 200, y: rect.midY - 72))
+        
+        path.addCurve(to: CGPoint(x: rect.midX - 100, y: rect.midY + 72), control1: CGPoint(x: rect.midX, y: rect.midY - 72), control2: CGPoint(x: rect.midX , y: rect.midY + 72))
+        return path
     }
 }
 
 struct InfinityShape_Previews: PreviewProvider {
     static var previews: some View {
         InfinityShape()
+            .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round))
     }
 }
