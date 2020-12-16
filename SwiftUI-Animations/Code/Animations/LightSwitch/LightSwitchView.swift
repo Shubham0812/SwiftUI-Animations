21ca700a89bd45765fc0ab6ed359d1349215afcd
Shubham0812 <singh.shubham0812@gmail.com>
2020-12-16 12:49:37 +0530

    LightSwitch animation added

---
+++ SwiftUI-Animations/Code/Animations/LightSwitch/LightSwitchView.swift
@@ -0,0 +1,21 @@
+//
+//  LightSwitchView.swift
+//  SwiftUI-Animations
+//
+//  Created by Shubham Singh on 16/12/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import SwiftUI
+
+struct LightSwitchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LightSwitchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LightSwitchView()
+    }
+}
